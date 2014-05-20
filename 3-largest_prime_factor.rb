56ffd4c2c81dee35285e4cac84de1f3282c3e9dd
Jisu Kim <jisu.kim@flatironschool.com>
2014-05-20 10:43:05 -0400

    Refactor solution

--- 3-largest_prime_factor.rb
+++ 3-largest_prime_factor.rb
@@ -5,13 +5,28 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 
-require 'prime'
+# require 'prime'
+
+# def largest_prime(x)
+#   largest_primes_array=Prime.first(5000).reverse
+
+#   largest_primes_array.each do |prime|
+#   #p prime
+#   return prime if x%prime == 0
+#   end
+# end
+
+
+#solution without using the prime class
 
 def largest_prime(x)
-  largest_primes_array=Prime.first(5000).reverse
-
-  largest_primes_array.each do |prime|
-  #p prime
-  return prime if x%prime == 0
+  prime_number = 2
+  while (prime_number <= x) do
+    if x%prime_number == 0
+      x=x/prime_number
+    else
+      prime_number+=1
+    end
   end
+  prime_number
 end
