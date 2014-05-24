473e8d0afddea4a4e1303261318f66cc1ea25778
Jisu Kim <jisu.kim@flatironschool.com>
2014-05-24 19:09:30 -0400

    Refactor solution

--- 10-summation_of_primes.rb
+++ 10-summation_of_primes.rb
@@ -1,7 +1,33 @@
-require 'prime'
+# Summation of primes
+# Problem 10
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-def summation_of_primes
-  sum=0 
-  Prime.each(2000000){|prime| sum+=prime }
-  puts sum
+# Find the sum of all the primes below two million.
+
+
+# require 'prime'
+
+# def summation_of_primes
+#   sum=0 
+#   Prime.each(2000000){|prime| sum+=prime }
+#   puts sum
+# end
+
+#solution2 without using prime class
+#create a prime? function and store value if it is prime
+#iterate through numbers 2..2 million and call prime function on it
+
+def prime?(number)
+  (2...number).each do |x|
+    return false if number % x == 0  
+  end
+  true
+end
+
+def sum_primes
+  primes = []
+  (2...2000000).each do |x|
+    p primes<<x if prime?(x)
+  end
+  primes.inject(:+)
 end
