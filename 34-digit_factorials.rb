9a708e0c9c7cf152b1500beb40f4946c119891d1
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-11 15:18:42 -0400

    Add solution to problem 34

--- 34-digit_factorials.rb
+++ 34-digit_factorials.rb
@@ -7,3 +7,27 @@
 
 # Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
+def curious_number?(x)
+  return false if x==1 || x==2
+
+  digits = x.to_s.split('').map(&:to_i)
+  factorials_of_digits=[]
+  digits.each do |x|  
+    factorial = 1
+    while(x>1)
+      factorial = factorial*x
+      x-=1
+    end
+    factorials_of_digits<<factorial
+  end
+
+  factorials_of_digits.inject(:+)==x ? true:false
+end
+
+def solution
+  curious_numbers=[]
+  (1..99999).each do |x|
+    curious_numbers<<x if curious_number?(x)
+  end
+  curious_numbers.inject(:+)
+end
