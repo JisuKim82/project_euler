fa64172729c96448f50b65e40fee2c9c390e50e0
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-11 15:13:02 -0400

    Add test specs

--- spec/34-digit_factorials_spec.rb
+++ spec/34-digit_factorials_spec.rb
@@ -1,3 +1,18 @@
 require_relative 'spec_helper'
 require_relative '../34-digit_factorials'
 
+describe '#curious_number?' do
+  describe 'the sum of all numbers which are equal to the sum of the factorial of their digits' do
+    it 'returns true for 145' do
+      expect(curious_number?(145)).to eq true
+    end
+
+    it 'returns false for 146' do
+      expect(curious_number?(146)).to eq false
+    end
+
+    it 'returns false for 2' do
+      expect(curious_number?(2)).to eq false
+    end
+  end
+end
