33456ac0cad67d1b09a11ea42dd3178afa033517
Jisu Kim <jisu.kim@flatironschool.com>
2014-06-15 19:10:30 -0400

    Refactor solution

--- 35-circular_primes.rb
+++ 35-circular_primes.rb
@@ -26,19 +26,54 @@ def circular_numbers(x)
   results
 end
 
-def prime?(number)
-  (2...number).each do |x|
-    return false if number % x == 0 && number != 2
+
+# Sieve of Eratosthenes to speed up th solution
+# The algorithm needs to have an upper limit for the primes to find. Lets call this limit N
+# The algorithm works as follows.
+
+# Create a list l of consecutive integers {2,3,…,N}.
+# Select p as the first prime number in the list, p=2.
+# Remove all multiples of p from the l.
+# set p equal to the next integer in l which has not been removed.
+# Repeat steps 3 and 4 until p^2 > N, all the remaining numbers in the list are primes
+
+def solution
+  prime = 2
+  index = 0
+
+  list = (2..1000000).to_a
+
+  while (prime**2 < 1000000)
+    list = list.select{|x| (x % list[index] != 0) || (list[index] == x)}
+    index += 1
+    prime = list[index]
   end
-  true
+
+  circular_primes = []
+  
+  list.each do |prime|
+    if circular_numbers(prime).map{|x| list.include?(x)}.all?{|x| x==true}
+      p circular_primes<<prime
+    end
+  end
+  circular_primes
 end
 
-circular_primes=[]
 
-(2..1000000).each do |number|
-  if circular_numbers(number).map{|x| prime?(x)}.all?{|x| x==true}
-    circular_primes<<number
-  end
-end
+
+# def prime?(number)
+#   (2...number).each do |x|
+#     return false if number % x == 0 && number != 2
+#   end
+#   true
+# end
+
+# circular_primes=[]
+
+# (2..1000000).each do |number|
+#   if circular_numbers(number).map{|x| prime?(x)}.all?{|x| x==true}
+#     circular_primes<<number
+#   end
+# end
 
 
