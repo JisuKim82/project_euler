19e37a7bb29889527ace88bd05cf8dc5e82a9f05
Jisu Kim <jisu.kim@flatironschool.com>
2014-10-18 12:39:12 -0400

    Add solution

--- merge_sort.rb
+++ merge_sort.rb
@@ -13,3 +13,26 @@
   #return result
 
 
+def merge_sort(arr)
+  return arr if arr.length<2
+
+  middle_idx = arr.length/2
+  left_arr = arr[0..(middle_idx-1)]
+  right_arr = arr[middle_idx..-1]
+
+  left_arr = merge_sort(left_arr)
+  right_arr = merge_sort(right_arr)
+  merge(left_arr,right_arr)
+end
+
+def merge(left, right)
+  result = []
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      result<<left.shift
+    else
+      result<<right.shift
+    end
+  end
+  result + left + right
+end
