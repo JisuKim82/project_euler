b01a5e6c2082ae9ea154fd41cd46bbc946a09050
Jisu Kim <jisu.kim@flatironschool.com>
2014-10-18 12:10:07 -0400

    Pseudocode for merge sort

---
+++ merge_sort.rb
@@ -0,0 +1,15 @@
+#Merge Sort
+
+#find middle index of array
+#split array in half and assign to left and right
+#recursive call to itself with the left side passed in
+#recursive call to itself with the right side passed in
+#merge the left and right arrays
+  #until either array is empty
+    #if left.first <= right.first
+      # result<<left.shift
+    #else result<<right.shift
+  #concatenate result with array elements left over
+  #return result
+
+
